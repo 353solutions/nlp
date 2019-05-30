e31e59cd3a97d9fc94564b899d0932f29d391116
Miki Tebeka <miki.tebeka@gmail.com>
2019-05-30 11:02:36 +0100

    using testify & modules

--- nlp_test.go
+++ nlp_test.go
@@ -3,8 +3,10 @@ package nlp
 import (
 	"encoding/json"
 	"os"
-	"reflect"
+	//	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 /* Place the test cases in tokenizer_cases.json
@@ -22,22 +24,30 @@ type TestCase struct {
 }
 
 func loadCases(t *testing.T) []TestCase {
+	require := require.New(t)
 	var testCases []TestCase
 
 	file, err := os.Open("tokenizer_cases.json")
-	if err != nil {
-		t.Fatalf("can't open test cases - %s", err)
-	}
+	require.NoError(err, "open test cases")
+	/*
+		if err != nil {
+			t.Fatalf("can't open test cases - %s", err)
+		}
+	*/
 	defer file.Close()
 
 	dec := json.NewDecoder(file)
-	if err := dec.Decode(&testCases); err != nil {
-		t.Fatalf("bad JSON in test cases - %s", err)
-	}
+	require.NoError(dec.Decode(&testCases), "bad JSON in test cases")
+	/*
+		if err := dec.Decode(&testCases); err != nil {
+			t.Fatalf("bad JSON in test cases - %s", err)
+		}
+	*/
 	return testCases
 }
 
 func TestTokenizeMulti(t *testing.T) {
+	require := require.New(t)
 	testCases := loadCases(t)
 
 	for _, tc := range testCases {
@@ -47,22 +57,30 @@ func TestTokenizeMulti(t *testing.T) {
 		}
 		t.Run(name, func(t *testing.T) {
 			out := Tokenize(tc.Text)
-			if !reflect.DeepEqual(tc.Expected, out) {
-				t.Fatalf("%#v != %#v", tc.Expected, out)
-			}
+
+			require.Equal(tc.Expected, out)
+			/*
+				if !reflect.DeepEqual(tc.Expected, out) {
+					t.Fatalf("%#v != %#v", tc.Expected, out)
+				}
+			*/
 		})
 
 	}
 }
 
 func TestTokenize(t *testing.T) {
+	require := require.New(t)
 	text := "Who's on first?"
 	out := Tokenize(text)
 	expected := []string{"who", "s", "on", "first"}
+	require.Equal(expected, out)
 
-	if !reflect.DeepEqual(expected, out) {
-		// See also t.Failf
-		// Using %#v will output more information
-		t.Fatalf("%#v != %#v", expected, out)
-	}
+	/*
+		if !reflect.DeepEqual(expected, out) {
+			// See also t.Failf
+			// Using %#v will output more information
+			t.Fatalf("%#v != %#v", expected, out)
+		}
+	*/
 }
